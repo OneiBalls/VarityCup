149b3557a46034e5f1d5cb3681d19944acde5200
Hyphrio <notmynn@proton.me>
2025-03-09 20:02:15 +0800

    Import font and some +layout.svelte cleanup

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,20 +1,24 @@
 <script lang="ts">
+	import '@fontsource-variable/funnel-display'
 	import '../app.css';
 	let { children } = $props();
 </script>
 
-<div class='h-screen w-screen bg-gray-400'>
-    <div class='text-3xl border border-red-400 border-dotted'>
-		<p>Pretend this is banner rawr</p>
-        <!-- Erm we put this as static? -->
-    </div>
+<header class="flex items-center justify-between m-4 ">
+	<p class="font-display font-bold text-2xl">Pretend this is banner rawr</p>
     <nav>
 		<a href="/">Info</a>
 		<a href="/standings">Standings</a>
 		<a href="/games">Games</a>
 		<a href="/teams">Teams</a>
 	</nav>
-    <div class='main'>
-		{@render children()}
-	</div> 
-</div>
+</header>
+
+<!-- Replace this with a UI theme -->
+<main class="border-black border-y-2">
+	{@render children()}
+</main>
+
+<footer>
+	aaaaaaaaaaaaaaaa
+</footer>
