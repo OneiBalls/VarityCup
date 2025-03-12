6656df2f6f81523adf2ecbab1f645dc6d876a166
Hyphrio <notmynn@proton.me>
2025-03-12 19:31:38 +0800

    I forgot to push the event route erm

---
+++ src/routes/event/+page.svelte
@@ -0,0 +1,22 @@
+<main class="flex justify-center py-4">
+    <ul class="menu menu-horizontal bg-base-200">
+        <li>
+            <details open>
+                <!-- Load this from DB, can be removed if this is only one-time -->
+                <summary>Event 1</summary>
+            </details>
+        </li>
+        <li>
+            <details open>
+                <!-- Mandatory, load from db -->
+                <summary>Game</summary>
+                <ul>
+                    <li><button>Minecraft</button></li>
+                    <li>Kahoot!</li>
+                    <li>Geoguessr</li>
+                    <li>Marvel Rivals</li>
+                </ul>
+            </details>
+        </li>
+    </ul>
+</main>
